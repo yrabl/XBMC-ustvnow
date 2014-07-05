b5f625a3a1ff901199ea4999c599d4833c03ba5f
durdle <ddurdle@gmail.com>
2014-07-04 21:43:08 -0400

    ready for release

--- default.py
+++ default.py
@@ -16,10 +16,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from resources.lib import Addon, ustvnow 
+from resources.lib import Addon, ustvnow
+from resources.lib import ustvnowPlayer
+from resources.lib import tvWindow
+
 import sys
 import urllib
-import xbmc, xbmcgui, xbmcplugin
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 
 Addon.plugin_url = sys.argv[0]
 Addon.plugin_handle = int(sys.argv[1])
@@ -35,19 +38,68 @@ Addon.log('plugin handle: ' + str(Addon.plugin_handle))
 
 mode = Addon.plugin_queries['mode']
 
+settingMode = Addon.get_setting('settingMode')
+controlMode = Addon.get_setting('controlMode')
+
+
+addon = xbmcaddon.Addon(id='plugin.video.ustvnow')
+
+
+#added by ddurdle
+try:
+
+    remote_debugger = Addon.get_setting('remote_debugger')
+    remote_debugger_host = Addon.get_setting('remote_debugger_host')
+
+    # append pydev remote debugger
+    if remote_debugger == 'true':
+        # Make pydev debugger works for auto reload.
+        # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+        import pysrc.pydevd as pydevd
+        # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        pydevd.settrace(remote_debugger_host, stdoutToServer=True, stderrToServer=True)
+except ImportError:
+    log(Addon.get_string(30016), True)
+    sys.exit(1)
+except :
+    pass
+
+
 if mode == 'main':
     Addon.log(mode)
+
+    if settingMode == 'true':
+        stream_type = ['rtmp', 'rtsp'][int(Addon.get_setting('stream_type'))]
+        channels = ustv.get_channels(int(Addon.get_setting('quality')),
+                                     stream_type)
+
+        player = ustvnowPlayer.ustvnowPlayer()
+        player.setChannels(channels)
+
+        w = tvWindow.tvWindow("tvWindow.xml",addon.getAddonInfo('path'),"Default")
+        w.setPlayer(player)
+        w.doModal()
+
+
     Addon.add_directory({'mode': 'live'}, Addon.get_string(30001))
     Addon.add_directory({'mode': 'recordings'}, Addon.get_string(30002))
 
 elif mode == 'live':
     Addon.log(mode)
     stream_type = ['rtmp', 'rtsp'][int(Addon.get_setting('stream_type'))]
-    channels = ustv.get_channels(int(Addon.get_setting('quality')), 
+    channels = ustv.get_channels(int(Addon.get_setting('quality')),
                                      stream_type)
-    for c in channels:
-        Addon.add_video_item(c['url'],
-                             {'title': '%s - %s' % (c['name'], 
+    if controlMode == 'true':
+
+        for c in channels:
+            Addon.add_directory({'mode': 'play', 'name': c['name']},
+                             '%s - %s' % (c['name'], c['now']['title']))
+
+
+    else:
+        for c in channels:
+            Addon.add_video_item(c['url'],
+                             {'title': '%s - %s' % (c['name'],
                                                     c['now']['title']),
                               'plot': c['now']['plot']},
                              img=c['icon'])
@@ -55,20 +107,20 @@ elif mode == 'live':
 elif mode == 'recordings':
     Addon.log(mode)
     stream_type = ['rtmp', 'rtsp'][int(Addon.get_setting('stream_type'))]
-    recordings = ustv.get_recordings(int(Addon.get_setting('quality')), 
+    recordings = ustv.get_recordings(int(Addon.get_setting('quality')),
                                      stream_type)
     for r in recordings:
-        cm_del = (Addon.get_string(30003), 
-                  'XBMC.RunPlugin(%s/?mode=delete&del=%s)' % 
+        cm_del = (Addon.get_string(30003),
+                  'XBMC.RunPlugin(%s/?mode=delete&del=%s)' %
                        (Addon.plugin_url, urllib.quote(r['del_url'])))
         title = '%s (%s on %s)' % (r['title'], r['rec_date'], r['channel'])
-        Addon.add_video_item(r['stream_url'], {'title': title, 
+        Addon.add_video_item(r['stream_url'], {'title': title,
                                                'plot': r['plot']},
                              img=r['icon'], cm=[cm_del], cm_replace=True)
 
 elif mode == 'delete':
     dialog = xbmcgui.Dialog()
-    ret = dialog.yesno(Addon.get_string(30000), Addon.get_string(30004), 
+    ret = dialog.yesno(Addon.get_string(30000), Addon.get_string(30004),
                        Addon.get_string(30005))
     if ret == 1:
         ustv.delete_recording(Addon.plugin_queries['del'])
@@ -80,15 +132,34 @@ elif mode=='play':
        channels = []
        quality = int(Addon.get_setting('quality'))
        channels = ustv.get_channels(quality,stream_type)
-       for c in channels:
-         if c['name'] == name:
-           print "URL = "+c['url']
-           print "setResolvedUrl"
-           item = xbmcgui.ListItem(path=c['url'])
-           xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+       if controlMode == 'true':
+
+         count = -1
+         for c in channels:
+           count += 1
+
+           Addon.add_directory({'mode': 'play', 'name': c['name']},
+                             '%s - %s' % (c['name'], c['now']['title']))
+           if c['name'] == name:
+
+                 player = ustvnowPlayer.ustvnowPlayer()
+                 player.setChannels(channels)
+                 player.setCurrent(count)
+
+                 w = tvWindow.tvWindow("tvWindow.xml",addon.getAddonInfo('path'),"Default")
+                 w.setPlayer(player)
+                 w.doModal()
+       else:
+           for c in channels:
+            if c['name'] == name:
+                 print "URL = "+c['url']
+                 print "setResolvedUrl"
+                 item = xbmcgui.ListItem(path=c['url'])
+                 xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
     except:
       pass
 
 Addon.end_of_directory()
-        
+
 
---
+++ resources/lib/ustvnowPlayer.py
@@ -0,0 +1,95 @@
+
+'''
+    ustvnow XBMC Plugin
+    Copyright (C) 2013 ddurdle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import re
+import urllib, urllib2
+
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin
+
+addon = xbmcaddon.Addon(id='plugin.video.ustvnow')
+
+def log(msg, err=False):
+    if err:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGERROR)
+    else:
+        xbmc.log(addon.getAddonInfo('name') + ': ' + msg.encode('utf-8'), xbmc.LOGDEBUG)
+
+
+class ustvnowPlayer(xbmc.Player):
+
+
+    def __init__( self, *args, **kwargs ):
+        xbmc.Player.__init__( self )
+        self.isExit = 0
+
+    def setChannels(self, channels):
+        self.channels = channels
+        self.current = 1
+
+    def setCurrent(self, current):
+        self.current = current
+
+    def up(self):
+        if self.current == len(self.channels) -1:
+            self.current = 1
+        else:
+            self.current += 1
+
+        self.playChannel()
+
+    def down(self):
+        if self.current == 1:
+            self.current = len(self.channels) -1
+        else:
+            self.current -= 1
+
+        self.playChannel()
+
+    def getChannelName(self):
+        return self.channels[self.current]['name']
+
+    def getShowName(self):
+        return self.channels[self.current]['now']['title']
+
+    def playChannel(self):
+        self.play(self.channels[self.current]['url'])
+
+    def setIsExit(self,value):
+        self.isExit = value
+
+    def onPlayBackStarted(self):
+        print "PLAYBACK STARTED"
+
+    def onPlayBackEnded(self):
+        print "PLAYBACK ENDED"
+        xbmc.sleep(1000)
+        if self.isExit == 0:
+            self.playChannel()
+
+    def onPlayBackStopped(self):
+        print "PLAYBACK STOPPED"
+        xbmc.sleep(1000)
+        if self.isExit == 0:
+            self.playChannel()
+
+
+    def onPlayBackPaused(self):
+        print "PLAYBACK Paused"
+
